16ba359bb98c20551da36ba735dd81eb6a0faf19
SeedPaths-User <lessdismissive@email.com>
2014-09-04 08:45:43 -0600

    Guess1

---
+++ GuessThatNumber/GuessThatNumber/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessThatNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberGame();
+        }
+        static void NumberGame()
+        {
+            
+            //variable for number of guesses
+            int counter = 0;
+            //random number generator
+            Random game = new Random();
+            int answer = game.Next(1, 101);
+            Console.WriteLine("Please guess a number between 1 and 100.");
+            bool winner = false;
+           
+            while (!winner)
+            {
+                //variable for user input
+                string input = Console.ReadLine();
+                //parse input to make it an int
+                int guess = int.Parse(input);
+                //make your commands for the game
+                if (answer == guess)
+                {
+                    winner = true;
+                    Console.WriteLine("Congratulations, you must be a lucky guesser!");
+                    counter++;
+                }
+                else if (answer < guess)
+                {
+                    Console.WriteLine("You're to low, guess higher!");
+                    counter++;
+                }
+                else if (answer > guess)
+                {
+                    Console.WriteLine("Oh no! You're to high, come down a bit.");
+                    counter++;
+                }
+                else
+                {
+                    Console.WriteLine("Please play the game right!");
+                    counter++;
+                }
+            }
+            Console.WriteLine("It took you " + counter + " guesses to win.");
+        }
+    }
+}
